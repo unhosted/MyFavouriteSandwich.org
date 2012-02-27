57fad5710d90ede2c6e9ded7148ce04c198e5c21
Michiel de Jong <michiel@unhosted.org>
2012-02-27 18:04:26 +0000

    clean up unused files, add tar ball, edit text in footer and on login page

---
+++ MyFavouriteSandwich/unhosted/remoteStorage-0.5.1.js
@@ -0,0 +1,154 @@
+define(
+  ['require', './lib/ajax.js', './lib/couch.js', './lib/dav.js', './lib/webfinger.js'], 
+  function (require, ajax, couch, dav, webfinger) {
+    var onError = function (code, msg) {
+        console.log(msg);
+      },
+      getStorageInfo = function (userAddress, cb) {
+        webfinger.getAttributes(
+          userAddress, {
+            allowHttpWebfinger: true,
+            allowSingleOriginWebfinger: false,
+            allowFakefinger: true
+          }, 
+          function (err, data) {
+            cb(err, null);
+          }, 
+          function (attributes) {
+            cb(0, attributes);
+            var storageAddresses = {};
+          }
+        );
+      }, 
+      createOAuthAddress = function (storageInfo, categories, redirectUri) {
+        var terms = [
+          'redirect_uri='+encodeURIComponent(redirectUri),
+          'scope='+encodeURIComponent(categories.join(',')),
+          'response_type=token',
+          'client_id='+encodeURIComponent(redirectUri)
+        ];
+        return storageInfo.auth + (storageInfo.auth.indexOf('?') === -1?'?':'&') + terms.join('&');
+      },
+      getDriver = function (api, cb) {
+        require([api === 'CouchDB'?'./lib/couch.js':'./lib/dav.js'], cb);
+      },
+      createClient = function (storageInfo, category, token) {
+        var storageAddress = webfinger.resolveTemplate(storageInfo.template, category);
+        return {
+          get: function (key, cb) {
+            getDriver(storageInfo.api, function (d) {
+              d.get(storageAddress, token, key, cb);
+            });
+          },
+          put: function (key, value, cb) {
+            getDriver(storageInfo.api, function (d) {
+              d.put(storageAddress, token, key, value, cb);
+            });
+          },
+          'delete': function (key, cb) {
+            getDriver(storageInfo.api, function (d) {
+              d['delete'](storageAddress, token, key, cb);
+            });
+          }
+        };
+      },
+      receiveToken = function () {
+        var params, kv;
+        /**
+          this needs more attention.
+        **/
+        if(location.hash.length > 0) { 
+          params = location.hash.split('&');
+          for(var i = 0, il = params.length; i < il; i++) {
+            if(params[i].length && params[i][0] ==='#') {
+              params[i] = params[i].substring(1);
+            }
+            kv = params[i].split('=');
+            if(kv.length >= 2) {
+              if(kv[0]==='access_token') {
+                ///XXX: ok im guessing its a base64 string and you somehow adding an = sign to the end of it ok, why?
+                var token = unescape(kv[1]);//unescaping is needed in chrome, otherwise you get %3D%3D at the end instead of ==
+                for(var j = 2,jl = kv.length; i < jl; i++) {
+                  token += '='+kv[i]; 
+                }
+                return token;
+              }
+            }
+          }
+        }
+        return null;
+      },
+      onReadyStateChange = function(cb) {
+        readyStateChangeHandler = cb;
+        if(localStorage['_unhosted$storageInfo'] && localStorage['_unhosted$bearerToken']) {
+          connected = true;
+        } else {
+          connected = false;
+        }
+        online = true;
+        ready = true;
+        readyStateChangeHandler(connected, online, ready);
+      },
+      readyStateChangeHandler = function() {},
+      connected = false,
+      online = false,
+      ready = false,
+      sync = function(userAddress, categories) {
+        var libPath = '/unhosted';
+        window.open(libPath+'/openDialog.html'
+          +'?userAddress='+encodeURIComponent(userAddress)
+          +'&categories='+encodeURIComponent(JSON.stringify(categories))
+          +'&libPath='+encodeURIComponent(libPath));
+        window.addEventListener('message', function(event) {
+          if(event.origin == location.protocol +'//'+ location.host) {
+            if(event.data.substring(0, 5) == 'conn:') {
+              var data = JSON.parse(event.data.substring(5));
+              localStorage['_unhosted$storageInfo'] = JSON.stringify(data.storageInfo);
+              localStorage['_unhosted$categoriesToSync'] = JSON.stringify(categories);
+              localStorage['_unhosted$bearerToken'] = data.bearerToken;
+              startSync();
+            }
+          }
+        }, false);
+      },
+      startSync = function() { 
+        var libPath = '/unhosted';
+        var storageInfo = JSON.parse(localStorage['_unhosted$storageInfo']);
+        connected = true;
+        var syncFrame = document.createElement('iframe');
+        syncFrame.setAttribute('style', 'border-style:none;width:1px;height:1px;');
+        syncFrame.src= location.protocol+'//'+location.host+libPath+'/syncFrame.html'
+          +'?api='+encodeURIComponent(storageInfo.api)
+          +'&template='+encodeURIComponent(storageInfo.template)
+          +'&categories='+encodeURIComponent(localStorage['_unhosted$categoriesToSync'])
+          +'&token='+encodeURIComponent(localStorage['_unhosted$bearerToken']);
+        document.body.appendChild(syncFrame);
+        window.addEventListener('message', function(event) {
+          if((event.origin == location.protocol +'//'+ location.host) && (event.data.substring(0, 5) == 'sync:')) {
+            ready = (event.data == 'sync:ready');
+            readyStateChangeHandler(connected, online, ready);
+          }
+        }, false);
+      },
+      disconnect = function() {
+        localStorage.clear();
+        connected = false;
+        readyStateChangeHandler(connected, online, ready);
+      };
+  if(localStorage['_unhosted$categoriesToSync']) {
+    try {
+      startSync();
+    } catch(e) {
+      console.log('could not sync categories '+localStorage['_unhosted$categoriesToSync']);
+    }
+  }
+  return {
+    getStorageInfo     : getStorageInfo,
+    createOAuthAddress : createOAuthAddress,
+    createClient       : createClient,
+    receiveToken       : receiveToken,
+    sync               : sync,
+    disconnect         : disconnect,
+    onReadyStateChange : onReadyStateChange
+  };
+});
---
+++ MyFavouriteSandwich/unhosted/sync.js
@@ -0,0 +1,100 @@
+var Syncer = function() {
+  var clients = {};
+  var indexCache = {};
+  var indexKey;
+  function getLocalIndex(category) {
+    if(indexCache[category]) {//this is necessary because localStorage contents is not updated instantly on write
+      return indexCache[category];
+    }
+    var str = localStorage[indexKey+'_index_'+category];
+    if(str) {
+      try {
+        indexCache[category] = JSON.parse(str);
+        return indexCache[category];
+      } catch (e) {
+      }
+    }
+    //build up index from scratch:
+    indexCache[category] = {};
+    for(var i = 0; i < localStorage.length; i++) {
+      var keyParts = localStorage.key(i).split('$');
+      if(keyParts.length == 2 && keyParts[0] == category) {
+        indexCache[category][localStorage.key(i)] = 0;
+      }
+    }
+    localStorage[indexKey+'_index_'+category] = JSON.stringify(indexCache[category]);
+    return indexCache[category];
+  }
+  function updateLocalIndex(category, key) {
+    getLocalIndex(category);//prime indexCache
+    indexCache[category][key] = new Date().getTime();
+    localStorage[indexKey+'_index_'+category] = JSON.stringify(indexCache[category]);
+  }
+  function init(setStorageInfo, setCategories, setToken, setIndexKey, cb) {
+    indexKey = setIndexKey;
+    require(['http://unhosted.org/remoteStorage-0.4.3.js'], function(remoteStorage) {
+      for(var i in setCategories) {
+        clients[setCategories[i]] = remoteStorage.createClient(setStorageInfo, setCategories[i], setToken);
+      }
+      sync(cb);
+    });
+  }
+  function sync(cb) {
+    if(!clients.length) {
+      cb();
+      return;
+    }
+    for(var category in clients) {
+      clients[category].get(indexKey, function(err, data) {
+        if((!err) && data) {
+          var remoteIndex = JSON.parse(data);
+          var localIndex = getLocalIndex(category);
+          JSON.parse(localStorage[clients[category]+'$'+indexKey]);
+          var key;
+          for(key in remoteIndex) {
+            if(!localIndex[key] || localIndex[key] < remoteIndex[key]) {
+              clients[i].get(key, function(err, data) {
+                updateLocalIndex(category, key);
+                localStorage[category+'$'+key] = data;
+              });
+            }
+          }
+          var putIndex = false;
+          for(key in localIndex) {
+            if(!remoteIndex[key] || remoteIndex[key] < localIndex[key]) {
+              putIndex = true;
+              clients[category].put(key, localStorage[category+'$'+key], function(err, data) {
+              });
+            }
+          }
+          //todo: deal with upload failures
+          clients[category].put(indexKey, JSON.stringify(localIndex), function(err, data) {
+          });
+        }
+      });
+    }
+    cb();//not really finished here yet actually
+  }
+  function push(e, cb) {
+    if(!e.key) {//this happens on localStorage.clear()
+      return;
+    }
+    var parts = e.key.split('$');
+    if(parts.length != 2) {
+      return;
+    }
+    var index = updateLocalIndex(parts[0], parts[1]);
+    if(clients[parts[0]]) {
+      clients[parts[0]].put(indexKey, JSON.stringify(getLocalIndex(parts[0])), function(err, data) {
+        clients[parts[0]].put(parts[1], e.newValue, function(err, data) {
+          cb();
+        });
+      });
+    }
+  }
+  return {
+    sync: sync, 
+    init: init,
+    push: push
+  };
+};
